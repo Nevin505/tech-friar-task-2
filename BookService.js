9fb02a8a765408e57bfcb838e719d11dd45d17cd
Nevin <nevinjohn63@gmail.com>
2024-08-11 17:26:08 +0530

    add quick book for phone

---
+++ BookService.js
@@ -0,0 +1,29 @@
+const elemnts=document.getElementsByClassName('pickUp-OptionContainer');
+
+const bookingDescription=document.getElementsByClassName('booking-type-container')
+
+// To Render View For Mobile Version
+const onlyForMobileView=document.getElementsByClassName('onlyForMobileView');
+
+const bookCarButton=document.getElementById('bookCar-button');
+// 
+
+bookCarButton.addEventListener('click',()=>{
+    const clonedNode=elemnts[0].cloneNode(true);
+    const bookingDescriptionCloneNode=bookingDescription[0].cloneNode(true);
+    // To Change the Value of the Drop Down Tag
+    const clonedSelectTag=clonedNode.querySelector('select');
+    clonedSelectTag.options[0].textContent  = 'Vehicle Type';
+   // To Change the text content value of the button via using class Name
+    const clonedSearchButtonElement=bookingDescriptionCloneNode.querySelector('.search-button');
+    clonedSearchButtonElement.textContent='Search'
+    clonedNode.classList.add('visbleBookOptions')
+    bookingDescriptionCloneNode.classList.add('changeLayout')
+    onlyForMobileView[0].innerHTML='';
+    onlyForMobileView[0].appendChild(clonedNode)
+    onlyForMobileView[0].appendChild(bookingDescriptionCloneNode);
+    console.log(clonedNode)
+    console.log(bookingDescriptionCloneNode)
+})
+// console.log(elemnts[0])
+// onlyForMobileView.appendChild(elemnts[0])
