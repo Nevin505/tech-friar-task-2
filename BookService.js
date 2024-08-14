3366787cee2f00a280f1dcf38a86e6455313c20b
Nevin <nevinjohn63@gmail.com>
2024-08-14 23:14:37 +0530

    added buttonOnclick for mobile version

--- BookService.js
+++ BookService.js
@@ -11,6 +11,7 @@ const bookCarButton=document.getElementById('bookCar-button');
 bookCarButton.addEventListener('click',()=>{
     const clonedNode=elemnts[0].cloneNode(true);
     const bookingDescriptionCloneNode=bookingDescription[0].cloneNode(true);
+
     // To Change the Value of the Drop Down Tag
     const clonedSelectTag=clonedNode.querySelector('select');
     clonedSelectTag.options[0].textContent  = 'Vehicle Type';
@@ -22,30 +23,16 @@ bookCarButton.addEventListener('click',()=>{
     onlyForMobileView[0].innerHTML='';
     onlyForMobileView[0].appendChild(clonedNode)
     onlyForMobileView[0].appendChild(bookingDescriptionCloneNode);
-    console.log(clonedNode)
     console.log(bookingDescriptionCloneNode)
 })
-// console.log(elemnts[0])
-// onlyForMobileView.appendChild(elemnts[0])
 
+const removePickUpLocation=()=>{
+    const sameAsPickUpButton = document.querySelector('.onlyForMobileView >.booking-type-container > #same-as-pickUp');
+  sameAsPickUpButton.style.display='none'
+}
 
-const getOptions=()=>{
-    const droppedElement=document.getElementsByClassName('dropLocation');
-    console.log(droppedElement[1]);
-    
-    console.log("Clicked");
-    onlyForMobileView[0].innerHTML=''
-    const clonedNode=elemnts[0].cloneNode(true);
-    const bookingDescriptionCloneNode=bookingDescription[0].cloneNode(true);
-    console.log(bookingDescriptionCloneNode);
-    
-    // bookingDescriptionCloneNode.removeChild(droppedElement[0])
-    const clonedSelectTag=clonedNode.querySelector('select');
-    clonedSelectTag.options[0].textContent  = 'Vehicle Type';
-    bookingDescriptionCloneNode.classList.add('changeLayout');
-    clonedNode.classList.add('visbleBookOptions')
-
-    onlyForMobileView[0].appendChild(clonedNode)
-    onlyForMobileView[0].appendChild(bookingDescriptionCloneNode);
-
+const addPickUpLocations=()=>{
+      const removeDifferentPickUp=document.querySelector('.onlyForMobileView >.booking-type-container > #same-as-pickUp');
+ 
+      removeDifferentPickUp.style.display='block'
 }
